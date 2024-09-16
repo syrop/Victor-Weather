6f36a3b5b4c8995afa2b41f23c30ea461865c353
wiktor <wiktor.nizio@crosslab.ch>
2024-09-16 11:56:21 +0200

    Added search icon.

--- app/src/main/java/pl/org/seva/victorweather/screen/CityScreen.kt
+++ app/src/main/java/pl/org/seva/victorweather/screen/CityScreen.kt
@@ -10,7 +10,11 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -24,8 +28,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.util.trace
 import androidx.navigation.NavHostController
 import pl.org.seva.victorweather.R
 import pl.org.seva.victorweather.destination.CityDetailsDestination
@@ -51,6 +57,16 @@ fun CityScreen(
         ) {
             TextField(
                 value = city,
+                trailingIcon = {
+                    IconButton(
+                        onClick = { cityPresentation.findCities(scope, city) },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = null,
+                        )
+                    }
+                },
                 onValueChange = {
                     val text = it.trim()
                     if (text.isEmpty() || Regex("^[a-zA-Z\\p{L}]+\$").matches(text))
